dbecf044749d67bdfa009863054a834183acd09a
Diane Mutako <dmutako@cs.brown.edu>
2019-04-23 00:14:04 -0400

    kmeans speed on NY done

--- Kmeans-speed.py
+++ Kmeans-speed.py
@@ -3,11 +3,7 @@ import csv
 import numpy.matlib
 import numpy as np
 import matplotlib.pyplot as plt
-# import pymysql
-# connection = pymysql.connect(host='35.196.53.153',
-#                              user='diane',
-#                              password='',
-#                              db='census2')
+
 def sklearn_cluster(X, Y, K):
 	kmeans = KMeans(K)
 	clusters = kmeans.fit_predict(X)
@@ -25,44 +21,29 @@ def elbow_point_plot(xdata, ydata):
 		k += 1
 	plt.plot(clusters, errors)
 	plt.show()
-import pdb
 def main ():
-	with open("../DS final project/NY-Fixed-Dec2017-v1.csv") as broaband_data:
-		csv_reader = csv.DictReader(broaband_data, delimiter = ',')
-		xdata = {}
-		ydata = []
-		for row in csv_reader:
-			if (row["Consumer"]):
-				key = row["BlockCode"][:-3]
-				if (key not in xdata.keys()):
-					xdata[key] = []
-				xdata[key].append(float(row["MaxAdDown"]))
-	with open("./New York Group Blkgrp Income Estimate.csv") as census_data:
-		csv_reader = csv.DictReader(census_data, delimiter = ',')
-		cdata = {}
-		for row in csv_reader:
-			key = row["STATEA"] + row["COUNTYA"] + row["TRACTA"] + row["BLKGRPA"]
-			cdata[key] = float(row["AH1OE001"])
 	speedData = []
-	labels = []
-	for key in xdata:
-		if key in cdata:
-			speedData.append(sum(xdata[key])/len(xdata[key]))
-			labels.append(cdata[key])
+	income = []
+	with open('./speed_income_NY.csv') as data:
+		for row in data:
+			row_data  = row.split(',')
+			if len(row_data) > 1:
+				speedData.append(float(row.split(',')[1]))
+				income.append(float(row.split(',')[0]))
 
-	k = 8
-	print(len(labels), 'length', '\n', len(cdata), '\n')
-	print(len(speedData), 'length', '\n', len(xdata))
-	# print(xdata)
-	#centroids, closest_centroids = sklearn_cluster(xdata, ydata, k)
-	# elbow_point_plot(xdata, ydata)
-	#plot_clusters(xdata, ydata, centroids, closest_centroids)
+	k = 9
+	xdata = np.array(speedData).reshape(-1, 1)
+	centroids, closest_centroids, error = sklearn_cluster(xdata, income, k)
+	plot_clusters(xdata, income, centroids, closest_centroids)
 def plot_clusters(xdata, ydata, centroids, centroid_indices):
-	colors = {0: 'red', 1: 'yellow', 2: 'blue', 3: 'green', 4: 'brown', 5:'orange', 6: 'pink', 7: 'black'}
+	colors = {0: 'red', 1: 'yellow', 2: 'blue', 3: 'green', 4: 'brown', 5:'orange', 6: 'pink', 7: 'black', 8: 'grey'}
 	color = [colors[l] for l in centroid_indices]
 	print('centroids: ', centroids)
 	fig, ax = plt.subplots()
 	plt.scatter(xdata, ydata, c=color)
+	plt.xlabel('Average Downstream Speed')
+	plt.ylabel('Total Household Income($k)')
+	plt.savefig('income_speed_clusters')
 	plt.show()
 if __name__ == '__main__':
 	main()
